50f379013bc23ea5a3fafe93d84e286a32edc459
Juan Daniel Martinez <jdmartinez@itparral.edu.mx>
2023-10-16 12:14:33 -0600

    Removed unused files

--- src/pages/api/post/[name].js
+++ src/pages/api/post/[name].js
@@ -5,11 +5,7 @@ import path from "path";
 export default async function handler(req, res) {
   const { name } = req.query;
   const filePath = path.join(process.cwd(), `src/posts/${name}.md`);
-
-  // const markdownPost = readFileSync(`./posts/${name}.md`, "utf-8");
   const markdownPost = readFileSync(filePath, "utf8");
-  // const posts = await fetch(filePath)
-  //   .then((response ) => response.json());
   const postObject = splitMetadataAndContent(markdownPost);
 
   res.setHeader("Content-Type", "application/json");
