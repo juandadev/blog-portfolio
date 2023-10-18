0c25186319faa42240f5111c300158712900ed01
Juan Daniel Martinez <jdmartinez@itparral.edu.mx>
2023-10-17 22:15:21 -0600

    New post Minor changes in headings font size

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -2,10 +2,12 @@ import Layout from "../components/Layout/Layout";
 import PostItem from "../components/PostItem/PostItem";
 import posts from "../utils/posts.json";
 
+import s from "../styles/Blog.module.scss";
+
 export default function Blog() {
   return (
     <Layout title="Blog">
-      <h1 className="mb-5">Blog</h1>
+      <h1 className={`mb-5 ${s.title}`}>Blog</h1>
       {posts.map((item) => (
         <PostItem item={item} key={item.slug} />
       ))}
